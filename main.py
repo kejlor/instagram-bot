0444cc7a7af857ec5bf4d3b6df8bad1393b7916e
kejlor <woj450@gmail.com>
2021-11-04 15:10:31 +0100

    Added empty line for correct python syntax in main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from bot import Bot
 
 bot = Bot("YOUR_LOGIN", "YOUR_PASSWORD", "PROFILE_YOU_WANT_TO_DOWNLOAD_PHOTOS")
 bot.open_profile()
-bot.scroll_down(2)
+bot.scroll_down(0)
 bot.download_images()
