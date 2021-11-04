4ea9e32c1ab5656de8a6fdbf31056a4565894a88
kejlor <woj450@gmail.com>
2021-11-04 17:17:21 +0100

    Put variables from global scope to the class. Wrote if functions return anything or not.

--- bot.py
+++ bot.py
@@ -1,23 +1,21 @@
 import os
 import time
 
-import selenium
 import wget as wget
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-PATH = "C:/Users/Kejlor/PycharmProjects/chromedriver.exe"
-SAVE_PATH = "D:/InstagramBot"
-
 
 class Bot:
+    PATH = "YOUR_PATH_FOR_WEBDRIVERS"
+    SAVE_PATH = "DESIRED_PATH_TO_SAVE_FILES"
 
     def __init__(self, login, password, keyword):
         self.login = login
         self.keyword = keyword
-        self.driver = webdriver.Chrome(PATH)
+        self.driver = webdriver.Chrome(self.PATH)
         self.driver.get("http://www.instagram.com/")
         username_field = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
@@ -31,7 +29,7 @@ class Bot:
 
         time.sleep(5)
         WebDriverWait(self.driver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Akceptuję wszystko")]'))).click()
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Accept all")]'))).click()
 
         time.sleep(5)
         WebDriverWait(self.driver, 2).until(
@@ -39,33 +37,33 @@ class Bot:
 
         time.sleep(7)
         WebDriverWait(self.driver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Nie teraz")]'))).click()
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not now")]'))).click()
         time.sleep(7)
         WebDriverWait(self.driver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Nie teraz")]'))).click()
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not now")]'))).click()
 
-    def open_profile(self):
+    def open_profile(self) -> None:
         time.sleep(5)
         self.driver.get(f"http://www.instagram.com/{self.keyword}/")
 
-    def open_explore_tags(self):
+    def open_explore_tags(self) -> None:
         time.sleep(5)
         self.driver.get(f"https://www.instagram.com/explore/tags/{self.keyword[1:]}/")
 
-    def check_what_to_open(self):
+    def check_if_it_is_profile_or_tag(self) -> None:
         time.sleep(5)
         if self.keyword[0] != '#':
             self.open_profile()
         else:
             self.open_explore_tags()
 
-    def scroll_down(self, n_scrolls):
+    def scroll_down(self, n_scrolls) -> None:
         time.sleep(5)
         for j in range(0, int(n_scrolls)):
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(5)
 
-    def download_images(self):
+    def download_images(self) -> None:
         anchors = self.driver.find_elements_by_tag_name('a')
         anchors = [a.get_attribute('href') for a in anchors]
         anchors = [a for a in anchors if str(a).startswith("https://www.instagram.com/p/")]
@@ -84,7 +82,7 @@ class Bot:
 
         images[:5]
 
-        path = SAVE_PATH
+        path = self.SAVE_PATH
         if self.keyword[0] == '#':
             path = os.path.join(path, self.keyword[1:] + "s")
         else:
@@ -103,22 +101,6 @@ class Bot:
             wget.download(image, save_as)
             counter += 1
 
-    def get_first_post(self):
+    def open_first_post(self):
         self.driver.find_element_by_class_name("eLAPa").click()
         time.sleep(3)
-
-    def nested_check(self):
-        try:
-            time.sleep(1)
-            nes_nex = self.driver.find_element_by_class_name("    coreSpriteRightChevron  ")
-            return nes_nex
-
-        except selenium.common.exceptions.NoSuchElementException:
-            return 0
-
-    def next_post(self):
-        try:
-            nex = self.driver.find_element_by_class_name("QBdPU ")
-            return nex
-        except selenium.common.exceptions.NoSuchElementException:
-            return 0
